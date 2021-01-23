823e46508a5e298e8b1f4d15cbdbc769504cbe4f
Uğur YAMAN ヅ <uguryaman06@hotmail.com>
2021-01-23 18:44:28 +0300

    Ders Sonu

---
+++ Calisma/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Calisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            string deger;
+
+            deger = (dolarDun < dolarBugun) ? "Artış oku." : "Azalış oku.";
+
+            Console.WriteLine(deger);
+
+
+
+            int gunler = 6;
+            switch (gunler)
+            {
+                case 1:
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Salı");
+                    break;
+                case 3:
+                    Console.WriteLine("Çarşamba");
+                    break;
+                case 4:
+                    Console.WriteLine("Perşembe");
+                    break;
+                case 5:
+                    Console.WriteLine("Cuma");
+                    break;
+                case 6:
+                    Console.WriteLine("Cumartesi");
+                    break;
+                case 7:
+                    Console.WriteLine("Pazar");
+                    break;
+                default:
+                    Console.WriteLine("HAFTADA 7 GÜN VARDIR. :D");
+                    break;
+            }
+
+
+            
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine("Merhaba Dünya");
+                i++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 10; i++) Döngülerde bir bir artar.
+            //for (int i = 1; i <= 10; i=i+1) Farklı yazım şeklidir. Bir bir artar.
+            //for (int i = 1; i <= 10; i=i+2) Döngülerde ikişer ikişer artar.
+            //for (int i = 1; i <= 10; i+=2) Farklı yazım şeklidir. İkişer ikişer artar.
+
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+
+            Console.WriteLine("");
+            //array - Dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı","Programlamaya Başlangıç İçin Temel Kurs","Java","Python","C#"}; 
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("For Bitti.");
+            Console.WriteLine("");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - Footer");
+
+            
+
+        }   
+         
+
+    }
+}
